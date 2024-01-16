7be17f2d4fd76741bf04340ea01ce95822a2a422
Diego ribeiro <Diegoh.developer@gmail.com>
2024-01-15 21:33:17 -0300

    feat: Iniciando componente de botão usando a lib restyle.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
+import { ThemeProvider, useTheme } from '@shopify/restyle';
+import { Theme, theme } from './src/theme/theme';
+import { Button } from './src/components/Button/Button';
 
 function App(): React.JSX.Element {
+
+  const {} = useTheme<Theme>();
   return (
+    <ThemeProvider theme={theme}>
     <SafeAreaView>
       <Text preset='headingLarge' style={{color:'red'}}>Diego</Text>
-      <Text preset='headingLarge' italic style={{color:'blue'}}>Costa</Text>
+
+    <View style={{
+      paddingHorizontal:16,
+    }}>
+    <Button title='Entrar'/>
+    </View>
     </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,24 @@
+import { TouchableOpacity } from "react-native";
+import { Text } from "../Text/Text";
+import { useTheme } from "@shopify/restyle";
+import { Theme } from "../../theme/theme";
+
+interface ButtonProps{
+    title:string;
+}
+export function Button({title}: ButtonProps){
+    const {colors} = useTheme<Theme>();
+    return (
+        <TouchableOpacity style={{
+            paddingHorizontal:20,
+            paddingVertical:14,
+            backgroundColor:colors.primary,
+            alignItems:'center',
+            borderRadius:16,
+        }}>
+            <Text style={{color:'#FFF'}}>
+                {title}
+            </Text>
+        </TouchableOpacity>
+    )
+}
