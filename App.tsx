4a338b225a3cead9e474a6752c35a662a9ea87be
Diego ribeiro <Diegoh.developer@gmail.com>
2024-01-31 14:02:51 -0300

    feat: iniciando tela de login.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, TextInput, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {ThemeProvider, useTheme} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Icon} from './src/components/Icon/Icon';
+import { Box } from './src/components/Box/box';
+import { Button } from './src/components/Button/Button';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{padding: 20}}>
-          <Text preset="headingLarge">Diego</Text>
 
-          <Icon name="eyeOn" color="carrotSecondary" size={25} />
-          <Icon name="eyeOff" color="backgroundContrast" size={50} />
+          <Text marginBottom='s8' preset="headingLarge">Olá</Text>
+          <Text preset='paragraphLarge' mb='s40'>Digite seu e-mail e senha para entrar </Text>
+          
+          <Box mb='s20'>
+          <TextInput placeholder='Digite seu e-mail' style={{borderWidth:1,height:50}}/>
+          </Box>
+          <Box >
+          <TextInput placeholder='Digite sua senha' style={{borderWidth:1,height:50}}/>
+          </Box>
+          <Text mt='s10' marginBottom='s8' color='primary' preset="paragraphSmall" bold>Esqueci minha senha</Text>
+
+          <Button mt='s48' title='Entrar'/>
+          <Button preset='outline' mt='s12' title='Criar uma conta'/>
+
         </View>
       </SafeAreaView>
     </ThemeProvider>
