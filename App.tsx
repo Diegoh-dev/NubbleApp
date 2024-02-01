7dff02c742c7974f821af5895b17b3b96c146c22
Diego ribeiro <Diegoh.developer@gmail.com>
2024-02-01 13:11:57 -0300

    feat: Componente de textInput.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {SafeAreaView, TextInput, View} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {ThemeProvider, useTheme} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Icon} from './src/components/Icon/Icon';
 import { Box } from './src/components/Box/box';
 import { Button } from './src/components/Button/Button';
+import { TextInput } from './src/components/TextInput/TextInput';
 
 function App(): React.JSX.Element {
   return (
@@ -13,14 +14,14 @@ function App(): React.JSX.Element {
       <SafeAreaView>
         <View style={{padding: 20}}>
 
-          <Text marginBottom='s8' preset="headingLarge">Olá</Text>
+          <Text marginBottom='s8' preset="headingLarge">Olá!</Text>
           <Text preset='paragraphLarge' mb='s40'>Digite seu e-mail e senha para entrar </Text>
           
           <Box mb='s20'>
-          <TextInput placeholder='Digite seu e-mail' style={{borderWidth:1,height:50}}/>
+          <TextInput label='E-mail' placeholder='Digite seu e-mail'/>
           </Box>
           <Box >
-          <TextInput placeholder='Digite sua senha' style={{borderWidth:1,height:50}}/>
+          <TextInput label='Senha' placeholder='Digite sua senha' />
           </Box>
           <Text mt='s10' marginBottom='s8' color='primary' preset="paragraphSmall" bold>Esqueci minha senha</Text>
 
