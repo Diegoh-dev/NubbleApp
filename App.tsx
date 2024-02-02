43a9f21d29b08ecb815d6b8b23d111c92174a46d
Diego ribeiro <Diegoh.developer@gmail.com>
2024-02-02 13:37:47 -0300

    feat:hook para calcular a safeArea.

--- App.tsx
+++ App.tsx
@@ -8,12 +8,15 @@ import { Box } from './src/components/Box/box';
 import { Button } from './src/components/Button/Button';
 import { TextInput } from './src/components/TextInput/TextInput';
 import { LoginScreen } from './src/screens/auth/LoginScreen/LoginScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function App(): React.JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-     <LoginScreen/>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <LoginScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,14 +1,21 @@
-import React, { ReactNode } from "react";
-import { Box } from "../Box/box";
+import React, {ReactNode} from 'react';
+import {Box} from '../Box/box';
+import {Platform} from 'react-native';
+import {useAppSafeArea} from '../../hooks/useAppSafeArea';
 
-interface ScreenProps{
-    children:ReactNode;
+interface ScreenProps {
+  children: ReactNode;
 }
 
-export function Screen({children}:ScreenProps){
-    return(
-        <Box paddingHorizontal="s24">
-            {children}
-        </Box>
-    )
+export function Screen({children}: ScreenProps) {
+  const {top} = useAppSafeArea();
+  console.log({
+    device: Platform.OS,
+    top,
+  });
+  return (
+    <Box paddingHorizontal="s24" style={{paddingTop: top}}>
+      {children}
+    </Box>
+  );
 }
