0972c37c651b3e9278a814c06eb2bbe57ad4ff49
Diego ribeiro <Diegoh.developer@gmail.com>
2024-02-04 20:51:20 -0300

    feat: configurando o React Navigation no app.

--- App.tsx
+++ App.tsx
@@ -4,19 +4,19 @@ import {Text} from './src/components/Text/Text';
 import {ThemeProvider, useTheme} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Icon} from './src/components/Icon/Icon';
-import { Box } from './src/components/Box/box';
-import { Button } from './src/components/Button/Button';
-import { TextInput } from './src/components/TextInput/TextInput';
-import { LoginScreen } from './src/screens/auth/LoginScreen/LoginScreen';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { SignUpScreen } from './src/screens/auth/SignUpScreen/SignUpScreen';
+import {Box} from './src/components/Box/box';
+import {Button} from './src/components/Button/Button';
+import {TextInput} from './src/components/TextInput/TextInput';
+import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SignUpScreen} from './src/screens/auth/SignUpScreen/SignUpScreen';
+import {Router} from './src/Routes/Routes';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        {/* <LoginScreen /> */}
-        <SignUpScreen/>
+        <Router />
       </ThemeProvider>
     </SafeAreaProvider>
   );
