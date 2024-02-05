60dc93c1983f79ee27cef3a70a0536e9d3c780c4
Diego ribeiro <Diegoh.developer@gmail.com>
2024-02-05 20:59:34 -0300

    refactor: removendo importações não usadas.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
-import {Text} from './src/components/Text/Text';
-import {ThemeProvider, useTheme} from '@shopify/restyle';
+import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
-import {Icon} from './src/components/Icon/Icon';
-import {Box} from './src/components/Box/box';
-import {Button} from './src/components/Button/Button';
-import {TextInput} from './src/components/TextInput/TextInput';
-import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {SignUpScreen} from './src/screens/auth/SignUpScreen/SignUpScreen';
 import {Router} from './src/Routes/Routes';
 
 
