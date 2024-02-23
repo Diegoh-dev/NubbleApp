7ec63ae6285d2e99b1e7d917233dc77dad7eb8ab
Diego ribeiro <Diegoh.developer@gmail.com>
2024-02-22 22:00:37 -0300

    feat: Iniando validação do formulário de signUp com a biblioteca zod.

--- src/screens/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -1,40 +1,36 @@
 import React from 'react';
 import { Screen } from '../../../components/Screen/Screen';
 import { Text } from '../../../components/Text/Text';
-import { TextInput } from '../../../components/TextInput/TextInput';
-import { Icon } from '../../../components/Icon/Icon';
 import { Button } from '../../../components/Button/Button';
-import { PassWordInput } from '../../../components/PasswordInput/PasswordInput';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../../Routes/Routes';
 import { useResetNavigationSuccess } from '../../../hooks/useResetNavigationSuccess';
 import { useForm, Controller } from 'react-hook-form';
 import { FormTextInput } from '../../../components/Form/FormTextInput';
 import { FormPassWordInput } from '../../../components/Form/FormPassWordInput';
+import { SignUpShema, signUpShema } from './SignUpShema';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 type SignUpScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUpScreen'>;
-type SignUpFormType = {
-  userName: string;
-  fullName: string;
-  email: string;
-  password: string;
-}
+
 
 export function SignUpScreen({ navigation }: SignUpScreenProps) {
 
   const { reset } = useResetNavigationSuccess();
 
-  const { control, formState, handleSubmit } = useForm<SignUpFormType>({
+  const { control, formState, handleSubmit } = useForm<SignUpShema>({
     defaultValues: {
       userName: '',
       fullName: '',
       email: '',
       password: '',
     },
-    mode: 'onChange'
+    mode: 'onChange',
+    resolver: zodResolver(signUpShema)
   });
 
-  function submitForm(formValues: SignUpFormType) {
+  function submitForm(formValues: SignUpShema) {
 
     console.log({
       formValues
@@ -57,9 +53,6 @@ export function SignUpScreen({ navigation }: SignUpScreenProps) {
       <FormTextInput
         control={control}
         name='userName'
-        rules={{
-          required: 'Username obrigatório'
-        }}
         label="Seu username"
         placeholder="@"
         boxProps={{ mb: 's20' }}
@@ -68,9 +61,6 @@ export function SignUpScreen({ navigation }: SignUpScreenProps) {
       <FormTextInput
         control={control}
         name='fullName'
-        rules={{
-          required: 'Nome obrigatório'
-        }}
         autoCapitalize='words'
         label="Nome Completo"
         placeholder="Digite seu nome completo"
@@ -80,13 +70,6 @@ export function SignUpScreen({ navigation }: SignUpScreenProps) {
       <FormTextInput
         control={control}
         name='email'
-        rules={{
-          required: 'E-mail obrigatório',
-          pattern: {
-            value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-            message: 'E-mail inválido',
-          },
-        }}
         label="E-mail"
         placeholder="Digite seu e-mail"
         boxProps={{ mb: 's20' }}
@@ -95,13 +78,6 @@ export function SignUpScreen({ navigation }: SignUpScreenProps) {
       <FormPassWordInput
         control={control}
         name='password'
-        rules={{
-          required: 'Senha obrigatória',
-          minLength: {
-            value: 8,
-            message: 'Senha deve ter no mínimo 8 caracteres'
-          }
-        }}
         boxProps={{ mb: 's10' }}
         label="Senha"
         placeholder="Digite sua senha"
---
+++ src/screens/auth/SignUpScreen/signUpShema.ts
@@ -0,0 +1,24 @@
+import {z} from 'zod';
+
+type SignUpFormType = {
+    userName: string;
+    fullName: string;
+    email: string;
+    password: string;
+  }
+  
+const userNameRegex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim;
+
+export const signUpShema = z.object({
+    userName:z.string().regex(userNameRegex,"username inválido").toLowerCase(),
+    fullName: z.string().min(5, "nome muito curto").max(50, "nome muito longo").transform(value => {
+        return value.split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join()
+    }),
+    email:z.string().email("email inválido"),
+    password:z.string().min(8,"Senha deve ter no mínimo 8 caracteres")
+})
+
+// zod vai inferir o tipo travez do objeto shema
+export type SignUpShema = z.infer<typeof signUpShema>;
