27b3e3322e37db062c1b9d1c024f18663f1ff3ba
Diego ribeiro <Diegoh.developer@gmail.com>
2024-04-03 22:43:19 -0300

    Feat: Introdução ao domínio - Post model

---
+++ src/domain/Post/portService.ts
@@ -0,0 +1,17 @@
+// CAMADA INTERMEDIARIA QUE VAI FAZER A CONEXÃO ENTRE API/REPOSITORIO;
+// O APP NÃO ACESSA DIRETO A API OU REPOSITORIO, ELE ACESSA A CAMADA DE SERVIÇO;
+// A UNICA COISA QUE O APP PRECISA SABER É QUE ELE PODE ACESSAR UM SERVIÇO QUE RETORNA UMA LISTA PARA ELE.
+
+import { postApi } from './postApi';
+import {Post} from './types';
+
+async function getList(): Promise<Post[]>{
+   const postList = await postApi.getList();
+
+   return postList;
+}
+
+// ESSE É O SERVICO QUE O APP/COMPONENTES VÃO UTILIZAR
+export const postService = {
+    getList,
+};
---
+++ src/domain/Post/postApi.ts
@@ -0,0 +1,13 @@
+// ARQUIVO RESPONSAVEL POR FAZER A CONEXÃO COM API
+
+import { postListMock } from "./postListMock";
+import { Post } from "./types";
+
+async function getList(): Promise<Post[]>{
+    //TODO: simular um delay na API
+    return postListMock;
+}
+
+export const postApi = {
+    getList,
+};
