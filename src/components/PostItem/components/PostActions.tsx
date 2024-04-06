8db3f430d9338f228303fb0ab8a84593d6a65cc0
Diego ribeiro <Diegoh.developer@gmail.com>
2024-04-05 23:13:06 -0300

    feat: Post actions.

---
+++ src/components/PostItem/components/PostActions.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+import {Post} from '@domain';
+
+import {Box, Icon, IconProps, Text, TouchableOpacityBox} from '@components';
+
+type Props = Pick<Post, 'reactionCount' | 'commentCount' | 'favoriteCount'>;
+export function PostActions({
+  reactionCount,
+  commentCount,
+  favoriteCount,
+}: Props) {
+  function likePost() {
+    //TODO: implement like post
+  }
+  function navigateToComments() {
+    //TODO: implement comment post
+  }
+  function favoritePost() {
+    //TODO: implement bookmark post
+  }
+  return (
+    <Box flexDirection="row" mt="s16">
+      <Item
+        marked
+        icon={{
+          default: 'heart',
+          marked: 'heartFill',
+        }}
+        onPress={likePost}
+        text={reactionCount}
+      />
+
+      <Item
+        marked
+        icon={{
+          default: 'comment',
+          marked: 'comment',
+        }}
+        onPress={navigateToComments}
+        text={commentCount}
+      />
+
+      <Item
+        marked={false}
+        icon={{
+          default: 'bookmark',
+          marked: 'bookmarkFill',
+        }}
+        onPress={favoritePost}
+        text={favoriteCount}
+      />
+    </Box>
+  );
+}
+
+interface ItemProps {
+  onPress: () => void;
+  marked: boolean;
+  icon: {
+    default: IconProps['name'];
+    marked: IconProps['name'];
+  };
+  text: number;
+}
+
+function Item({onPress, icon, text, marked}: ItemProps) {
+  return (
+    <TouchableOpacityBox
+      flexDirection="row"
+      alignItems="center"
+      mr="s24"
+      onPress={onPress}>
+      <Icon
+        name={marked ? icon.marked : icon.default}
+        color={marked ? 'market' : undefined}
+      />
+      {text > 0 && (
+        <Text preset="headingSmall" ml="s4" bold>
+          {text}
+        </Text>
+      )}
+    </TouchableOpacityBox>
+  );
+}
