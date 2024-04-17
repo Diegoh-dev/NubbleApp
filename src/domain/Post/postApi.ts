d28ce31d043fa63ebc98bc5819c4fef9dd4f9ba9
Diego ribeiro <Diegoh.developer@gmail.com>
2024-04-16 22:38:38 -0300

    feat: Camada de API

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,32 +1,26 @@
 // ARQUIVO RESPONSAVEL POR FAZER A CONEXÃO COM API
 
-import {postListMock} from './postListMock';
-import {Post} from './types';
+import {PageAPI} from '@api';
 
-async function getList(): Promise<Post[]> {
+import {PostAPI} from './postTypes';
+
+
+async function getList(): Promise<PageAPI<PostAPI>> {
   //TODO: simular um delay na API
-    // endereço ip
+  // endereço ip
   //172.27.64.1:3333
-  try {
-    let response = await fetch('http://172.27.64.1:3333/user/post', {
-      method: 'GET',
-      headers: {
-        Authorization:
-          'Bearer MzQ.VC8JqsDiCWToOg5k2DcFSqkJXe-euyvOkE5JLO3VklQXJy9OqqtvOdgXe6YZ',
-      },
-    });
+  let response = await fetch('http://172.27.64.1:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer MzQ.VC8JqsDiCWToOg5k2DcFSqkJXe-euyvOkE5JLO3VklQXJy9OqqtvOdgXe6YZ',
+    },
+  });
 
-    let data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-  //   let data = await response.json();
-  //   console.log(data);
+  let data: PageAPI<PostAPI> = await response.json();
+  console.log(data);
 
-  // await new Promise((resolve) => setTimeout(()=>{resolve('')} ,1000));
-
-  return postListMock;
+  return data;
 }
 
 export const postApi = {
--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -2,16 +2,16 @@
 // O APP NÃO ACESSA DIRETO A API OU REPOSITORIO, ELE ACESSA A CAMADA DE SERVIÇO;
 // A UNICA COISA QUE O APP PRECISA SABER É QUE ELE PODE ACESSAR UM SERVIÇO QUE RETORNA UMA LISTA PARA ELE.
 
-import { postApi } from './postApi';
-import {Post} from './types';
+import {postApi} from './postApi';
+import {Post} from './postTypes';
 
-async function getList(): Promise<Post[]>{
-   const postList = await postApi.getList();
+async function getList(): Promise<Post[]> {
+  const postList = await postApi.getList();
 
-   return postList;
+  return postList;
 }
 
 // ESSE É O SERVICO QUE O APP/COMPONENTES VÃO UTILIZAR
 export const postService = {
-    getList,
+  getList,
 };
