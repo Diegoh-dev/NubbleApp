a35a3fba15668fc50efa17dee6c91dcaaf489118
Diego ribeiro <Diegoh.developer@gmail.com>
2024-04-16 22:01:03 -0300

    .

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,16 +1,34 @@
 // ARQUIVO RESPONSAVEL POR FAZER A CONEXÃO COM API
 
-import { postListMock } from "./postListMock";
-import { Post } from "./types";
+import {postListMock} from './postListMock';
+import {Post} from './types';
 
-async function getList(): Promise<Post[]>{
-    //TODO: simular um delay na API
+async function getList(): Promise<Post[]> {
+  //TODO: simular um delay na API
+    // endereço ip
+  //172.27.64.1:3333
+  try {
+    let response = await fetch('http://172.27.64.1:3333/user/post', {
+      method: 'GET',
+      headers: {
+        Authorization:
+          'Bearer MzQ.VC8JqsDiCWToOg5k2DcFSqkJXe-euyvOkE5JLO3VklQXJy9OqqtvOdgXe6YZ',
+      },
+    });
 
-    await new Promise((resolve) => setTimeout(()=>{resolve('')} ,1000));
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+  //   let data = await response.json();
+  //   console.log(data);
 
-    return postListMock;
+  // await new Promise((resolve) => setTimeout(()=>{resolve('')} ,1000));
+
+  return postListMock;
 }
 
 export const postApi = {
-    getList,
+  getList,
 };
