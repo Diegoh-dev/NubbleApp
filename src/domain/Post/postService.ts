1e18c7a1056654444761f1db2c61f56c78595238
Diego ribeiro <Diegoh.developer@gmail.com>
2024-04-17 22:46:57 -0300

    feat: Aplicando Adapter (Desing Patterns).

--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -2,13 +2,14 @@
 // O APP NÃO ACESSA DIRETO A API OU REPOSITORIO, ELE ACESSA A CAMADA DE SERVIÇO;
 // A UNICA COISA QUE O APP PRECISA SABER É QUE ELE PODE ACESSAR UM SERVIÇO QUE RETORNA UMA LISTA PARA ELE.
 
+import { postAdapter } from './postAdapter';
 import {postApi} from './postApi';
 import {Post} from './postTypes';
 
 async function getList(): Promise<Post[]> {
-  const postList = await postApi.getList();
+  const postPageApi = await postApi.getList();
 
-  return postList;
+  return postPageApi.data.map(postAdapter.toPost);
 }
 
 // ESSE É O SERVICO QUE O APP/COMPONENTES VÃO UTILIZAR
