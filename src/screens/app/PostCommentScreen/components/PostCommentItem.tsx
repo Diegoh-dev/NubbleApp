bd4ac610caf375c65b67a049f8c666d1af6cb188
Diego ribeiro <Diegoh.developer@gmail.com>
2024-04-28 21:38:09 -0300

    feat:Estado Global (Zustand).

--- src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
+++ src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Alert, Pressable} from 'react-native';
 
 import {PostComment, postCommentService, usePostCommentRemove} from '@domain';
-import { useToast } from '@services';
+import { useToastService } from '@services';
 
 import {Box, ProfileAvatar, Text} from '@components';
 
@@ -15,7 +15,7 @@ interface Props {
 
 export function PostCommentItem({postComment,onRemoveComment,userId,postAuthorId}: Props) {
 
-  const {showToast} = useToast();
+  const {showToast} = useToastService();
   const {mutate} = usePostCommentRemove({
     onSuccess:()=>{
       onRemoveComment();
--- src/services/toast/useToast.tsx
+++ src/services/toast/useToast.tsx
@@ -1,8 +1,32 @@
+import {ToastService} from './toastTypes';
+// import {useToastContext} from './useToastContext';
+import {useToastServiceZustand, useToastZustend} from './useToastZustend';
 
+export function useToast(): ToastService['toast'] {
+  // return useToastContext();
 
-import { ToastService } from './toastTypes';
-import { useToastContext } from './useToastContext';
+  // const {toast} = useToastContext();
+  // return toast;
 
-export function useToast(): ToastService {
-  return useToastContext();
+  return useToastZustend();
+  
+}
+
+export function useToastService(): Pick<
+  ToastService,
+  'showToast' | 'hideToast'
+> {
+  // const {showToast, hideToast} = useToastContext();
+
+  // return {
+  //   showToast,
+  //   hideToast,
+  // };
+
+  const {showToast, hideToast} = useToastServiceZustand();
+
+  return {
+    showToast,
+    hideToast,
+  };
 }
