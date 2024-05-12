36d6c3073b3a1bcc6f32ba736c89ba1cd582b920
Diego ribeiro <Diegoh.developer@gmail.com>
2024-05-11 21:50:16 -0300

    feat: Persistência de Dados com Async Storage e Zustand

--- src/services/authCredentials/useAuthCredentials.ts
+++ src/services/authCredentials/useAuthCredentials.ts
@@ -1,4 +1,7 @@
 import {create} from 'zustand';
+import {persist} from 'zustand/middleware';
+
+import {storage} from '../storage';
 
 import {AuthCredentialsService} from './authCredentialsType';
 
@@ -6,9 +9,17 @@ export function useAuthCrendentials(): AuthCredentialsService {
   return useAuthCredentialsZustand();
 }
 
-const useAuthCredentialsZustand = create<AuthCredentialsService>(set => ({
-  authCredentials: null,
-  saveCrendentials: async ac => set({authCredentials: ac}),
-  removeCrendentials: async () => set({authCredentials: null}),
-  isLoading: false,
-}));
+const useAuthCredentialsZustand = create<AuthCredentialsService>()(
+  persist(
+    set => ({
+      authCredentials: null,
+      saveCrendentials: async ac => set({authCredentials: ac}),
+      removeCrendentials: async () => set({authCredentials: null}),
+      isLoading: false,
+    }),
+    {
+      name: '@Auth',
+      storage: storage, //usando a mesma interface que o zustend espera receber
+    },
+  ),
+);
---
+++ src/services/storage/implementation/asyncStorage.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {Storage} from '../storage';
+
+export const asyncStorage: Storage = {
+  getItem: async key => {
+    const item = await AsyncStorage.getItem(key);
+    if (item) {
+      return JSON.parse(item);
+    }
+    return null;
+  },
+  setItem: async (key, valor) => {
+    await AsyncStorage.setItem(key, JSON.stringify(valor));
+  },
+  removeItem: async key => await AsyncStorage.removeItem(key),
+};
