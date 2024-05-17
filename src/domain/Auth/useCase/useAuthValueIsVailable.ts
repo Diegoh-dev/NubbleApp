5f0fd9e40f81e925f41e65ee46665261dd35401f
Diego ribeiro <Diegoh.developer@gmail.com>
2024-05-16 22:43:04 -0300

    feat: DRY - Hook Generics para username e email

--- src/domain/Auth/useCase/useAuthValueIsVailable.ts
+++ src/domain/Auth/useCase/useAuthValueIsVailable.ts
@@ -1,31 +1,61 @@
 import {QueryKeys} from '@infra';
 import {useQuery} from '@tanstack/react-query';
 
-import { useDebounce } from '@hooks';
+import {useDebounce} from '@hooks';
 
 import {authService} from '../AuthService';
 
-interface Params {
-  username: string;
-  enabled:boolean;
+interface Params<T extends {length: number}>{
+  value: T;
+  enabled: boolean;
+  queryKey:QueryKeys
+  isAvailableFunc: (value:T) =>  Promise<boolean>;
 }
-export function useAuthIsUsernameIsVailable({username,enabled}: Params) {
 
-    const debouncedUserName = useDebounce(username,1500);
+
+ function useAuthIsValueIsVailable<T extends {length: number}>({
+  value,
+  queryKey,
+  isAvailableFunc,
+  enabled,
+}: Params<T>) {
+  const debouncedValue = useDebounce(value, 1500);
 
   const {data, isFetching} = useQuery({
-    queryKey: [QueryKeys.IsUsernameAvailable, debouncedUserName],
-    queryFn: () => authService.isUsernameAvailable(debouncedUserName),
+    queryKey: [queryKey, debouncedValue],
+    queryFn: () => isAvailableFunc(debouncedValue),
     staleTime: 20000,
-    enabled: enabled && debouncedUserName.length > 0,
+    enabled: enabled && debouncedValue.length > 0,
   });
 
   // para mostrar para o usuário que ainda está fazendo a validação(carregamento) dos dados no debounce
 
-  const isDebouncing = debouncedUserName !== username;
+  const isDebouncing = debouncedValue !== value;
 
   return {
-    isUnavailable:data === false,
+    isUnavailable: data === false,
     isFetching: isFetching || isDebouncing,
   };
+}
+
+export function useAuthIsUsernameIsVailable({username, enabled}: {username:string,enabled:boolean}) {
+
+  return useAuthIsValueIsVailable({
+    value: username,
+    enabled,
+    isAvailableFunc: authService.isUsernameAvailable,
+    queryKey: QueryKeys.IsUsernameAvailable,
+  });
+
+}
+
+
+export function useAuthIsEmailIsVailable({email, enabled}: {email:string,enabled:boolean}) {
+
+  return useAuthIsValueIsVailable({
+    value: email,
+    enabled,
+    isAvailableFunc: authService.isEmailAvailable,
+    queryKey: QueryKeys.IsEmailAvailable,
+  });
 }
--- src/infra/infraTypes.ts
+++ src/infra/infraTypes.ts
@@ -3,4 +3,5 @@ export enum QueryKeys {
   PostCommentList = 'PostCommentList',
   UserGetById = 'UserGetById',
   IsUsernameAvailable = 'isUsernameAvailable',
+  IsEmailAvailable = 'IsEmailAvailable',
 }
