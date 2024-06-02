38ee24b08cc6d3706fdf1535c7e5691722efeab8
Diego ribeiro <Diegoh.developer@gmail.com>
2024-06-02 20:00:35 -0300

    feat: Encontre e Interaja com elementos (queries e events);

--- src/components/Button/__test__/Button.test.tsx
+++ src/components/Button/__test__/Button.test.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
-import {render} from 'test-utils';
+import {render, fireEvent} from 'test-utils';
 
 import {Button} from '../Button';
 describe('<Button />', () => {
-  it('the component rendered', () => {
-    render(<Button title="Teste render" />);
+  it('calls the onPress function when is pressed', () => {
+    // Cria uma função simulada.
+    const mockedOnPress = jest.fn();
+    const {getByText} = render(
+      <Button title="Button Title" onPress={mockedOnPress} />,
+    );
+
+    //https://testing-library.com/docs/react-testing-library/cheatsheet/
+    const titleElement = getByText(/button title/i);
+
+    // vai simular o click no botão "button title"
+    fireEvent.press(titleElement);
+    // espera que a função tenha sido chamada
+    expect(mockedOnPress).toHaveBeenCalled();
   });
-  it('should shows loading indicator', () => {
-    render(<Button title="Teste render" />);
-  });
+
+  // it('should shows loading indicator', () => {
+  //   render(<Button title="Teste render" />);
+  // });
 });
