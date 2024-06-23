ad1554493605045a359b1f7aab6364df2c95bfa6
Diego ribeiro <Diegoh.developer@gmail.com>
2024-06-23 17:01:44 -0300

    feat:Flaky Test e Debug em Código Assíncrono;

--- src/infra/hooks/__tests__/usePaginatedList.test.ts
+++ src/infra/hooks/__tests__/usePaginatedList.test.ts
@@ -6,7 +6,7 @@ import {usePaginatedList} from '../usePaginatedList';
 const page1 = ['item1', 'item2', 'item3'];
 const page2 = ['item4', 'item5', 'item5'];
 
-function geList(page: number): Promise<Page<string>> {
+async function getList(page: number): Promise<Page<string>> {
   const data = page === 1 ? page1 : page2;
 
   const meta: MetaDataPage = {
@@ -22,19 +22,33 @@ function geList(page: number): Promise<Page<string>> {
   return Promise.resolve({data, meta});
 }
 
+const mokeGetList = jest.fn(getList);
+
 describe('usePaginatedList', () => {
   it('returns all pages together and stops fetching if there are no more page', async () => {
     // retornar todas as paginas juntas e para de buscar se não hover mais paginas;
-    const {result} = renderHook(() => usePaginatedList(['key'], geList));
+    const {result} = renderHook(() => usePaginatedList(['key'], mokeGetList));
     //https://callstack.github.io/react-native-testing-library/docs/api#waitfor
     //https://jestjs.io/docs/expect#tostrictequalvalue
-    await waitFor(() => expect(result.current.list).toStrictEqual(page1));
+    await waitFor(() => expect(result.current.list).toStrictEqual(page1), {
+      timeout: 30000000,
+    });
 
     result.current.fetchNextPage();
 
-  await waitFor(() =>
-    expect(result.current.list).toStrictEqual([...page1, ...page2]),
-  );
+    await waitFor(
+      () => expect(result.current.list).toStrictEqual([...page1, ...page2])
+    );
+
+    //MANTER LIST [...page1, ...page2]
+    //GETLIST NÃO É CHAMADO NOVAMENTE
+    result.current.fetchNextPage();
+    //https://www.datadoghq.com/knowledge-center/flaky-tests/
+    await waitFor(
+      () => expect(result.current.list).toStrictEqual([...page1, ...page2])
+    );
+
+    expect(mokeGetList).toHaveBeenCalledTimes(2);
 
   });
 });
