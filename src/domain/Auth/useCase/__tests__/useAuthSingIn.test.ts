fff438919b74123fb47f11d0313efdb36177ccb8
Diego ribeiro <Diegoh.developer@gmail.com>
2024-06-23 15:59:55 -0300

    feat:Custom renderHook e queryClient;

--- src/domain/Auth/useCase/__tests__/useAuthSingIn.test.ts
+++ src/domain/Auth/useCase/__tests__/useAuthSingIn.test.ts
@@ -1,5 +1,4 @@
-import {renderHook} from '@testing-library/react-native';
-import {AllTheProviders, waitFor} from 'test-utils';
+import {renderHook, waitFor} from 'test-utils';
 
 import {authService} from '../../AuthService';
 import {useAuthSingIn} from '../useAuthSingIn';
@@ -27,11 +26,11 @@ describe('useAuthSingIn', () => {
       .spyOn(authService, 'signIn')
       .mockResolvedValueOnce(mockedAuthCredentials);
 
-    const {result} = renderHook(() => useAuthSingIn({
-      onSuccess:mockOnSuccess,
-    }), {
-      wrapper: AllTheProviders,
-    });
+    const {result} = renderHook(() =>
+      useAuthSingIn({
+        onSuccess: mockOnSuccess,
+      }),
+    );
 
     result.current.SignIn({
       email: 'diegoh.developer@gmail.com',
@@ -43,20 +42,20 @@ describe('useAuthSingIn', () => {
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
     expect(mockSaveCrendentials).toHaveBeenCalledWith(mockedAuthCredentials);
     expect(mockOnSuccess).toHaveBeenCalledWith(mockedAuthCredentials);
-
   });
 
-  it('calls the onError function with a message if sign-in fails', async() => {
-
+  it('calls the onError function with a message if sign-in fails', async () => {
     const mockOnError = jest.fn();
-//https://jestjs.io/docs/mock-function-api#mockfnmockrejectedvaluevalue
-    jest.spyOn(authService,'signIn').mockRejectedValue(new Error('invalid user'));
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const {result} = renderHook(() => useAuthSingIn({
-      onError:mockOnError,
-    }), {
-      wrapper: AllTheProviders,
-    });
+    //https://jestjs.io/docs/mock-function-api#mockfnmockrejectedvaluevalue
+    jest
+      .spyOn(authService, 'signIn')
+      .mockRejectedValue(new Error('invalid user'));
+
+    const {result} = renderHook(() =>
+      useAuthSingIn({
+        onError: mockOnError,
+      }),
+    );
 
     result.current.SignIn({
       email: 'diegoh.developer@gmail.com',
@@ -66,6 +65,5 @@ describe('useAuthSingIn', () => {
     await waitFor(() => expect(result.current.isError).toBe(true));
 
     expect(mockOnError).toHaveBeenCalledWith('invalid user');
-
   });
 });
--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -3,33 +3,41 @@ import React, {ReactNode} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from '@shopify/restyle';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {RenderOptions, render} from '@testing-library/react-native';
+import {
+  RenderHookOptions,
+  RenderOptions,
+  render,
+  renderHook,
+} from '@testing-library/react-native';
 
 import {theme} from '@theme';
-// REFERÊNCIA
-//https://testing-library.com/docs/react-testing-library/setup/#custom-render
+export const wrapperAllProviders = () => {
+  // REFERÊNCIA
+  //https://testing-library.com/docs/react-testing-library/setup/#custom-render
+  // para garantir que cada teste vai criar um novo queryClient
+  //dessa forma o queryClient não vai mais ser compartilhado entre diferentes testes;
+  //https://tanstack.com/query/v4/docs/framework/react/guides/testing#our-first-test
+  const queryClient = new QueryClient({
+    logger: {
+      log: console.log,
+      warn: console.warn,
+      // ✅ no more errors on the console for tests
+      //@ts-ignore
+      error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
+    },
+    defaultOptions: {
+      queries: {
+        retry: false,
+        cacheTime: Infinity,
+      },
+      mutations: {
+        retry: false,
+        cacheTime: Infinity,
+      },
+    },
+  });
 
-const queryClient = new QueryClient({
-  logger: {
-    log: console.log,
-    warn: console.warn,
-    // ✅ no more errors on the console for tests
-    //@ts-ignore
-    error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
-  },
-  defaultOptions: {
-    queries: {
-      retry: false,
-      cacheTime:Infinity,
-    },
-    mutations: {
-      retry: false,
-      cacheTime:Infinity,
-    },
-  },
-});
-export const AllTheProviders = ({children}: {children: ReactNode}) => {
-  return (
+  return ({children}: {children: ReactNode}) => (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <NavigationContainer>{children}</NavigationContainer>
@@ -42,9 +50,21 @@ function customRender<T = unknown>(
   component: React.ReactElement<T>,
   options?: Omit<RenderOptions, 'wrapper'>,
 ) {
-  return render(component, {wrapper: AllTheProviders, ...options});
+  return render(component, {wrapper: wrapperAllProviders(), ...options});
+}
+
+function customRenderHook<Result, Props>(
+  renderCallback: (props: Props) => Result,
+  options?: Omit<RenderHookOptions<Props>, 'wrapper'>,
+) {
+  return renderHook(renderCallback, {
+    wrapper: wrapperAllProviders(),
+    ...options,
+  });
 }
 
 export * from '@testing-library/react-native';
 
 export {customRender as render};
+
+export {customRenderHook as renderHook};
