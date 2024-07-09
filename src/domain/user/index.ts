5319689ca02184b7e18b55b5896109055cddb830
Diego ribeiro <Diegoh.developer@gmail.com>
2024-07-08 21:36:27 -0300

    feat:Busca de Usuário: Caso de Uso com Paginação.

--- src/domain/user/index.ts
+++ src/domain/user/index.ts
@@ -2,3 +2,4 @@ export * from './userTypes';
 export * from './userApi';
 export * from './userAdapter';
 export * from './useCase/useUserGetById';
+export * from './useCase/useUserSearch';
--- src/screens/app/SearchScreen/SearchScreen.tsx
+++ src/screens/app/SearchScreen/SearchScreen.tsx
@@ -1,21 +1,32 @@
 import React, {useState} from 'react';
 
+import { useUserSearch } from '@domain';
+
 import {Icon, Screen, Text, TextInput} from '@components';
+import { useDebounce } from '@hooks';
 import {AppScreenPros} from '@routes';
 
 export function SearchScreen({}: AppScreenPros<'SearchScreen'>) {
   const [search, setSearch] = useState('');
+  const debaounceSearch =  useDebounce(search);
+
+ const {list} = useUserSearch(debaounceSearch);
+
   return (
     <Screen
       canGoBack
       HeaderComponent={
         <TextInput
+          placeholder="Digite sua busca"
           onChangeText={setSearch}
           value={search}
-          leftComponent={<Icon color='gray3' name="search" />}
+          leftComponent={<Icon color="gray3" name="search" />}
         />
       }>
-      <Text>Search Screen</Text>
+        
+      {list.map(user => (
+        <Text key={user.id}>{user.userName}</Text>
+      ))}
     </Screen>
   );
 }
