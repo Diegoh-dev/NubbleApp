c096e05dee259245e97e3b6c8a8c65010aec2a89
Diego ribeiro <Diegoh.developer@gmail.com>
2024-08-05 22:10:19 -0300

    feat:Acessando Fotos: Android, IOS e setup Jest. Função de permissão no andoid para acesso de imagens da galeria.

--- src/screens/app/NewPostScreen/NewPostScreen.tsx
+++ src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import {Image} from 'react-native';
+
+import {useCameraRoll} from '@services';
 
 import {Screen, Text} from '@components';
-import { AppTabScreenProps } from '@routes';
+import {AppTabScreenProps} from '@routes';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function NewPostScreen({navigation}:AppTabScreenProps<'NewPostScreen'>) {
+
+export function NewPostScreen({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  navigation,
+}: AppTabScreenProps<'NewPostScreen'>) {
+  const {list} = useCameraRoll();
+  console.log({
+    list,
+  })
   return (
-    <Screen>
+    <Screen scrollable>
       <Text> My Post Screen</Text>
+      {list.map(photo => (
+        <Image source={{uri: photo}} style={{width: 200, height: 200}} />
+      ))}
     </Screen>
   );
 }
