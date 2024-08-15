755fdd7c4f74ce7a78ad852b4220949ae26314ec
Diego ribeiro <Diegoh.developer@gmail.com>
2024-08-14 22:26:49 -0300

    feat: criando as permissões de acesso a camera e galeria em dispositivos ANDROID (permissionService)

--- src/services/permission/permissionService.ts
+++ src/services/permission/permissionService.ts
@@ -1,14 +1,55 @@
 // FUNÇÃO QUE VERIFICA SE A PERMISSÃO ESTÁ CONCEDIDA OU NÃO
 
+import { Permission, PermissionsAndroid, Platform } from 'react-native';
+
 import {PermissionName, PermissionService, PermissionStatus} from './permissionTypes';
 
 async function check(name:PermissionName):Promise<PermissionStatus> {
+    const permission = mapNamePermission(name);
+    if (permission){
+        const result = await PermissionsAndroid.check(permission);
 
+        if (result){
+            return 'granted';
+        }
+
+        return 'denied';
+    }
+
+    return 'unavailable';
 }
 
 // FUNÇÃO QUE VAI SER CHAMADA NA PRIMEIRA VEZ PARA REQUISITAR A PERMISSÃO
-async function request(name: PermissionName)Promise<PermissionStatus> {}
+async function request(name: PermissionName): Promise<PermissionStatus> {
+  const permission = mapNamePermission(name);
+  if (permission) {
+    const result = await PermissionsAndroid.request(permission);
+    return result;
+  }
+
+  return 'unavailable';
+}
+
+
 export const permissionService: PermissionService = {
     request,
     check,
 };
+
+
+//função para mapear o nome das permisões
+
+function mapNamePermission(name:PermissionName):Permission | null{
+    switch (name) {
+        case 'photoLibrary':
+            if (Number(Platform.Version) >= 33){
+                return 'android.permission.READ_MEDIA_IMAGES';
+            } else {
+                return 'android.permission.READ_EXTERNAL_STORAGE';
+            }
+            case 'camera' :
+                return 'android.permission.CAMERA';
+        default:
+          return null;
+    }
+}
