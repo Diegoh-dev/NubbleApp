2a90420aa8b357c31a510e7f29e3c35635af605d
Diego ribeiro <Diegoh.developer@gmail.com>
2024-11-27 21:55:20 -0300

    feat:Importação das imagens do onboarding do app.

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,11 @@
 export const images = {
   imagePlaceholder: require('./images/image_placeholder.png'),
+  onboardingLight1: require('./images/onboarding/onboarding-light-1.png'),
+  onboardingLight2: require('./images/onboarding/onboarding-light-2.png'),
+  onboardingLight3: require('./images/onboarding/onboarding-light-3.png'),
+  onboardingDark1: require('./images/onboarding/onboarding-dark-1.png'),
+  onboardingDark2: require('./images/onboarding/onboarding-dark-2.png'),
+  onboardingDark3: require('./images/onboarding/onboarding-dark-3.png'),
 };
+
+// OBS: @2x @3x é apenas um inticativo para o react native saber qual imagem ele vai escolher de acordo com a densidade do dispositivo.
